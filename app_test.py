414c523e9c884024100a66dbfef7fd187728c71a
Alex Bull <alexbull1999@gmail.com>
2024-12-22 20:26:40 +0000

    Adding pytests to pipeline

---
+++ app_test.py
@@ -0,0 +1,13 @@
+from app import app
+import pytest
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
